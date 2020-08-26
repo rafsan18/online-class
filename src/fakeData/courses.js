60b0e7dbf3ff6cbaece3272401c2251557ecd042
rafsan <rafsanamin18@gmail.com>
2020-08-27 01:28:19 +0600

    fake data created

---
+++ src/fakeData/courses.js
@@ -0,0 +1,94 @@
+var courses = [
+  {
+    name: "SEO 2020: Complete Guide",
+    price: 19.99,
+    img: "https://i.ibb.co/mG2sSdh/seo.jpg",
+    id: 201,
+  },
+  {
+    name: "The Complete Ruby on Rails Developer Course",
+    price: 123.99,
+    img: "https://i.ibb.co/KDq09tX/ruby-on-rails.jpg",
+    id: 202,
+  },
+  {
+    name: "React: The Complete Guide  ",
+    price: 69.99,
+    img: "https://i.ibb.co/WzXvQkc/react-js.png",
+    id: 203,
+  },
+  {
+    name: "Learn Python Programming Masterclass",
+    price: 89.99,
+    img: "https://i.ibb.co/120xhPK/python.png",
+    id: 204,
+  },
+  {
+    name: "PHP for Beginners",
+    price: 59.99,
+    img: "https://i.ibb.co/cXzBxCQ/php.pn",
+    id: 205,
+  },
+  {
+    name: "Adobe Photoshop CC: Complete Beginner to Advance Guide",
+    price: 79.99,
+    img: "https://i.ibb.co/xHSF44z/photoshop.png",
+    id: 206,
+  },
+  {
+    name: "Maya for Beginners",
+    price: 139.99,
+    img: "https://i.ibb.co/f1sDNbv/maya.jpg",
+    id: 207,
+  },
+  {
+    name: "Complete Guide of Lightroom",
+    price: 129.99,
+    img: "https://i.ibb.co/ZdYFVLT/lightroom.png",
+    id: 208,
+  },
+  {
+    name: "Java Programming Masterclass",
+    price: 169.99,
+    img: "https://i.ibb.co/Xjw64rG/java.jpg",
+    id: 209,
+  },
+  {
+    name: "Complete JavaScript Course 2020 ",
+    price: 99.99,
+    img: "https://i.ibb.co/BBKN86b/javascript.jpg",
+    id: 210,
+  },
+  {
+    name: "Complete Front-End Web Development Guide",
+    price: 119.99,
+    img: "https://i.ibb.co/Wpky7FS/front-end-development.jpg",
+    id: 211,
+  },
+  {
+    name: "Learn Content Writing",
+    price: 29.99,
+    img: "https://i.ibb.co/MpnM6HJ/content-writing.png",
+    id: 212,
+  },
+  {
+    name: "Complete AutoCad Training Course 2021",
+    price: 69.99,
+    img: "https://i.ibb.co/j46S2B0/autodesk-autocad.png",
+    id: 213,
+  },
+  {
+    name: "Complete Logo Design Course Using Adobe Illustrator: 2020",
+    price: 49.99,
+    img: "https://i.ibb.co/LRBkvvz/adobe-illustrator.jpg",
+    id: 214,
+  },
+  {
+    name: "Beginners to Advance Guide on Angular JS",
+    price: 79.99,
+    img: "https://i.ibb.co/vJs2GZc/angular-js.jpg",
+    id: 215,
+  },
+];
+
+export default courses;
